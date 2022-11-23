eb108db5c516cec20c46c734770b840eb6910ca4
Im-Jota <joseluisromeroarce12@gmail.com>
2022-11-23 17:02:29 +0000

    JS-II-final

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,7 +5,13 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
-
+    if(x > y){
+        return x;
+    } else if(x == y){
+        return y;
+    } else {
+        return y;
+    }
 }
 
 function mayoriaDeEdad(edad) {
@@ -148,7 +154,17 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
-    
+    if(num1 < 0 || num2 < 0 || num3 < 0){
+        return 'Hay negativos';
+    } else if(num1 > num2 && num1 > num3 && num1 > 0){
+        return 'Número 1 es mayor y positivo';
+    } else if(num1 == 0 || num2 == 0 || num3 == 0) {
+        return 'Error';
+    } else if(num3 > num1 && num3 > num2){
+        return num3 +=1;
+    } else {
+        return false;
+    }
 }
 
 function esPrimo(numero) {
@@ -216,7 +232,17 @@ function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
-    
+    let i = 0;
+    function au(numero){
+        do {
+            i++;
+            numero = numero + 5;
+        }while(i < 8);
+
+        return numero;
+    }
+
+    return au(numero);
 }
 
 
